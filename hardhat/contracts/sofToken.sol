e026f623b7be8661137768a4ada80bd8c8ebe0df
Harsh <harshnag23@gmail.com>
2023-03-23 20:50:17 +0530

    Contract almost done

--- hardhat/contracts/sofToken.sol
+++ hardhat/contracts/sofToken.sol
@@ -1,44 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/security/Pausable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 
-contract sofToken is
-    ERC1155,
-    Ownable,
-    Pausable,
-    ERC1155Burnable,
-    ERC1155Supply
-{
-    // deccalring varialbes section
-    struct softlicense {
-        uint256 cid; //ipfs cid
-        uint256 tokenId; //unique token id
-        uint256 price; // the price for the license
-        uint256[] dependentId; //array conatins the cid that this licence is dependent upon
-        uint256 quantity; //the amount of NFT license that need to be minted
-        bool isFirstNFT;
-        bool mintedSucessfully;
-    }
-    struct User {
-        address userAddress; //address of the user
-        uint256[] NFTsBought; // array of tokenid of the NFT this user has brought
-        uint256[] NFTsSold; //array of tokenid of NFT this user has sold
-        uint256[] NFTsMinted; //array of tokenid of NFT's this user has minted
-        uint256 countNFTsOwned; //count of NFT that this user owns
-    }
-    mapping(address => softlicense) userLicinseMap; //mapping of useraddress to the liceinse struct
-    mapping( address => User) userMap;  
-    uint256 idcount=0;  //to maintain the token id number when a NFT is minted
+contract Web3Builders is ERC1155, Ownable, Pausable, ERC1155Supply {
+    uint256 private total_tokens = 0;
+    uint256 private min_token_value = 0.01 ether;
+    uint256 private permitted_number_of_tokens = 1;
 
-    // construtor
-    constructor() ERC1155("") {}
+    mapping(uint256 => address) private token_to_creator_address;
+    mapping(address => uint256[]) private address_to_created_token;
+    mapping(string => uint256) private cid_to_token;
+    mapping(address => uint256[]) private address_to_subscribed_token;
+    mapping(address => mapping(uint256 => uint256)) private user_token_database;
+    mapping(uint256 => uint256[]) dependent_database;
+    mapping(uint256 => uint256) total_cost_per_token;
 
-    // Functions of the contract
+    constructor()
+        ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
+    {}
+
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
@@ -51,42 +36,102 @@ contract sofToken is
         _unpause();
     }
 
-// function to mint NFTs
     function mint(
-        address account,
-        uint256 id,
-        uint256 quantity,
-        bytes memory data,
-        address userAddress
+        string memory cid,
+        uint256 cost_of_token,
+        string[] memory dependent_cid
     ) public payable {
         require(
-            msg.value >= userLicinseMap[userAddress].quantity*quantity,
-            "insufficient funds to mint NFT of your software"
-        );  //if account funds are more than the amount of NFT then only he will be able to mint
-        _mint(msg.sender, idcount, quantity, data);
-        
-        userLicinseMap[userAddress].mintedSucessfully=true;
-        
-        
+            msg.value >= cost_of_token,
+            "Minimum token value of 0.01 ether is peermitted."
+        );
 
+        _mint(msg.sender, total_tokens, permitted_number_of_tokens, "");
+
+        token_to_creator_address[total_tokens] = msg.sender;
+        address_to_created_token[msg.sender].push(total_tokens);
+        cid_to_token[cid] = total_tokens;
+        user_token_database[msg.sender][total_tokens] = 1;
+
+        for (uint256 i = 0; i < dependent_cid.length; i++) {
+            uint256 token_id = cid_to_token[dependent_cid[i]];
+            dependent_database[total_tokens].push(token_id);
+        }
+
+        total_cost_per_token[total_tokens] =
+            total_cost_per_token[total_tokens] +
+            msg.value;
+        total_tokens++;
     }
 
-    // dependentLicense param contains the token ids that this nft is dependent on
-    function dependentNFT(uint256[] memory dependentLiciense, address userAddress) public{
-        for(uint256 i=0;i<dependentLiciense.length;i++){
-            userLicinseMap[userAddress].dependentId.push(dependentLiciense[i]);
+    function subscribe(
+        string memory cid,
+        uint256 cost_of_token
+    ) public payable {
+        uint256 token_id = cid_to_token[cid];
+        require(
+            user_token_database[msg.sender][token_id] == 0,
+            "User have already subscribed to this codebase."
+        );
+        require(
+            msg.value == cost_of_token,
+            "Incorrect amount of ether is sent."
+        );
+
+        _mint(msg.sender, token_id, permitted_number_of_tokens, "");
+        address_to_subscribed_token[msg.sender].push(token_id);
+        user_token_database[msg.sender][token_id] = 1;
+        total_cost_per_token[token_id] =
+            total_cost_per_token[token_id] +
+            msg.value;
+    }
+
+    function getCreatedTokensIds() public view returns (uint256[] memory) {
+        return address_to_created_token[msg.sender];
+    }
+
+    function getSubscribedTokenIds() public view returns (uint256[] memory) {
+        return address_to_subscribed_token[msg.sender];
+    }
+
+    function withdraw(string memory cid) external {
+        uint256 token_id = cid_to_token[cid];
+        require(
+            token_to_creator_address[token_id] == msg.sender,
+            "You are not the owner of this NFT."
+        );
+
+        uint256 number_of_dependent_owners = dependent_database[token_id]
+            .length;
+        if (number_of_dependent_owners == 0) {
+            uint256 cost_to_owner = total_cost_per_token[token_id];
+            payable(msg.sender).transfer(cost_to_owner);
+        } else {
+            uint256 total_cost_to_be_split = total_cost_per_token[token_id] /
+                10;
+            uint256 cost_to_owner = total_cost_per_token[token_id] -
+                total_cost_to_be_split;
+            uint256 individual_cost = total_cost_to_be_split /
+                number_of_dependent_owners;
+
+            for (uint256 i = 0; i < number_of_dependent_owners; i++) {
+                address dependent_owner = token_to_creator_address[
+                    dependent_database[token_id][i]
+                ];
+                payable(dependent_owner).transfer(individual_cost);
+            }
+            payable(msg.sender).transfer(cost_to_owner);
         }
     }
 
-// operations to perform if NFT is minted successfully
-    function NFTMinted(address userAddress) public {
-        if( userLicinseMap[userAddress].dependentId.length==0){
-            userLicinseMap[userAddress].isFirstNFT=true;
-        }
-        if(userLicinseMap[userAddress].mintedSucessfully==true){
-            userMap[userAddress].NFTsBought.push(idcount);
-            idcount++;
-        }
+    function uri(
+        uint256 _id
+    ) public view virtual override returns (string memory) {
+        require(exists(_id), "URI: nonexistent token");
+        return
+            string(
+                abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json")
+            );
     }
 
     function mintBatch(
